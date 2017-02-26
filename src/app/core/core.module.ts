c35ef024f9e7e8f99b672e1641b6cb8e8a294593
wudengguang <wudengguang@isa.qa>
2017-02-26 17:15:55 +0800

    引入bootstrap

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,8 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+  Optional,
+  SkipSelf
+} from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -19,4 +23,12 @@ import {
     AppComponent
   ]
 })
-export class CoreModule { }
+
+// 防止其他模块注入本模块
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the App Module only');
+    }
+  }
+}
