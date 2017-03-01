22a919245e072318aa96e7de36dedef62cc4b85c
wudengguang <wudengguang@isa.qa>
2017-03-01 21:47:25 +0800

    修改headbar

--- src/app/pages/sidebar/sidebar.constant.ts
+++ src/app/pages/sidebar/sidebar.constant.ts
@@ -117,5 +117,81 @@ export const PAGES_MENU = [
         path: '智能表格'
       }
     ]
+  },
+  {
+    title: '表格',
+    path: 'tables',
+    icon: 'fa-tag',
+    children: [
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      }
+    ]
+  },
+  {
+    title: '表格',
+    path: 'tables',
+    icon: 'fa-tag',
+    children: [
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      }
+    ]
+  },
+  {
+    title: '表格',
+    path: 'tables',
+    icon: 'fa-tag',
+    children: [
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      }
+    ]
+  },
+  {
+    title: '表格',
+    path: 'tables',
+    icon: 'fa-tag',
+    children: [
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      },
+      {
+        title: '智能表格',
+        path: '智能表格'
+      }
+    ]
   }
 ];
